4e17f30cc7c8dd3e8f119426bbe6ec16aeb4ecd4
aabasimel <abasimelahmed@gmail.com>
2025-09-07 17:11:36 +0200

    Delete alx_travel_app/

--- alx_travel_app/alx_travel_app/alx_travel_app/alx_travel_app/listings/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Welcome to Listings!")
